8892af08c045f241b59e0ccc786c312f44cbb8d4
ThanasisGeorg <thanasisgeorg03@gmail.com>
2023-06-15 17:33:56 +0300

    new file

---
+++ tic-tac-toe.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"    |    |    \n";
+    cout<<"____|____|____\n";
+    cout<<"    |    |    \n";
+    cout<<"  "space"\n";
+    cout<<"____|____|____\n";
+    cout<<"    |    |    \n";
+    cout<<"    |    |    \n";
+
+    return 0;
+}
